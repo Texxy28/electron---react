f693f680fda97786b5f636a31a87e104852cc1a0
Wilfredo Ortiz López <willyortilopezaeiou@gmail.com>
2023-01-14 20:25:46 -0500

    termine todo lo que queria con el searchbar, bien o no, se hizo:)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -23,6 +23,8 @@ const SearchBar = ({ placeholder, selectedOption, white, settings }) => {
 
     const [toogleX, setToogleX] = useState(false);
     const [bordePadre, setBordePadre] = useState(false)
+    const [selected, setSelected] = useState(false)
+    const [first, setfirst] = useState(false)
 
     const eliminarValue = () => {
         const input = document.querySelector('.searchbar_div-input');
@@ -30,25 +32,24 @@ const SearchBar = ({ placeholder, selectedOption, white, settings }) => {
         setToogleX(false);
     }
 
+    const selectInput = () => {
+        const input = document.querySelector('.searchbar_div-input');
+        input.focus();
+    }
+        
     const value = () => {
         const input = document.querySelector('.searchbar_div-input');
+        const hola = document.querySelector('.searchbar_div');
         if (input.value.length === 0) {
             setToogleX(false)
             setBordePadre(true)
+            hola.style.border = '1px solid #8b3dff'
         } else if (input.value.length !== 0) {
             setToogleX(true)
             setBordePadre(false)
+            hola.style.border = '1px solid #424345'
         }
     }
-
-    useEffect(() => {
-        const div = document.querySelector('.searchbar_div');
-        if (bordePadre) {
-            div.style.border = '#8b3dff solid 1px'
-        } else if (!bordePadre) {
-            div.style.border = '#424345 solid 1px'
-        }
-    }, [bordePadre])
     
 
     useEffect(() => {
@@ -72,20 +73,41 @@ const SearchBar = ({ placeholder, selectedOption, white, settings }) => {
 
     }, [toogleX]);
     
+    useEffect(() => {
+      
+        const hola = document.querySelector('.searchbar_div');
+        if (bordePadre && hola) {
+            hola.style.border = '1px solid #8b3dff'
+        } else if (!bordePadre && hola) {
+            console.log('dasd')
+            hola.style.border = '1px solid #424345'
+        }
+
+    }, [bordePadre])
 
     return (
         
         <div 
-            className='searchbar_div'
+            className={`searchbar_div ${white ? 'white' : 'black'}`}
             style={{
-                background: white === true ? '#fff' : '#18191b',
+                border: first ? !selected ? '1px solid #cfcfcf' : '' : '',
                 gridTemplateColumns: settings === true ? 'repeat(3,1fr)' : 'repeat(2,1fr)'
             }} 
+            onMouseEnter={
+                () => {
+                    setfirst(true)
+                }
+            }
+            onMouseLeave={
+                () => {
+                    setfirst(false)
+                }
+            }
         >
             
-            <div className='searchbar_div-searchicon'><BiSearch size={25} color='#d6d6d7' /></div>
+            <div className='searchbar_div-searchicon' onClick={() => selectInput()}><BiSearch size={20} color='#d6d6d7' /></div>
             <input 
-                className='searchbar_div-input' 
+                className='searchbar_div-input'
                 type='text' 
                 placeholder={result[0].placeholder} 
                 style={{
@@ -96,22 +118,40 @@ const SearchBar = ({ placeholder, selectedOption, white, settings }) => {
                         value()
                     }
                 }
+                onKeyDown={
+                    () => {
+                        value()
+                    }
+                }
                 onFocus={
                     () => {
                         value()
                         setBordePadre(true)
+                        setSelected(true)
                     } 
                 }
                 onBlur={
                     () => {
                         value()
                         setBordePadre(false)
+                        setSelected(false)
                     }
                 }
             />
 
-            {toogleX && <div className='searchbar_div-xicon' onClick={() => eliminarValue()}><IoMdClose size={25} color='#d6d6d7' /></div>}
-            {settings && <div className='searchbar_div-settingsicon'><VscSettings size={25} color='#d6d6d7'/></div>}
+            {toogleX && <div 
+                            className='searchbar_div-xicon' 
+                            onClick={
+                                () => {
+                                    eliminarValue()
+                                    selectInput()
+                                }
+                            }
+                        >
+                            <IoMdClose size={20} color='#d6d6d7' />
+                        </div>
+            }
+            {settings && <div className='searchbar_div-settingsicon'><VscSettings size={20} color='#d6d6d7'/></div>}
 
         </div>
         
